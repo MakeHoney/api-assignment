3866004dafa52e87906e887c1cdb3d2957c84946
makehoney <makehoney3@gmail.com>
2019-01-15 18:45:22 +0900

    initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import api from './api'
+
+const app = express()
+
+app.set('port', process.env.PORT || 8888)
+// app.set('jwt-secret', config.secret)
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ urlencoded: true }))
+
+// app.use('/', api)
+
+app.listen(app.get('port'), () => {
+  console.log(`Server Running on port ${app.get('port')}`)
+})
