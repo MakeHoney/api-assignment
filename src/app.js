2b4f0f1c99d5909e0ea68f8fc41cd9af06526e0f
makehoney <makehoney3@gmail.com>
2019-01-15 20:10:46 +0900

    Change port number

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ import api from './api'
 
 const app = express()
 
-app.set('port', process.env.PORT || 8888)
+app.set('port', process.env.PORT || 3000)
 // app.set('jwt-secret', config.secret)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ urlencoded: true }))
 
-// app.use('/', api)
+app.use('/api', api)
 
 app.listen(app.get('port'), () => {
   console.log(`Server Running on port ${app.get('port')}`)
